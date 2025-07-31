f48841097f868bfaebda9fd461e7390295ae73b6
Serhii Lavrenov <sergeylavrenov93@gmail.com>
2025-08-01 02:16:11 +0300

    reading counter started

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,4 @@
 "use strict";
-window.alert("Check the file \"popup.ts\"");
+window.alert("Popup opened");
+console.log("testing log");
 //# sourceMappingURL=popup.js.map
---
+++ scripts/content.js
@@ -0,0 +1,49 @@
+"use strict";
+function renderReadingTime(article) {
+    var _a, _b;
+    // If we weren't provided an article, we don't need to render anything.
+    if (!article) {
+        return;
+    }
+    const text = (_a = article.textContent) !== null && _a !== void 0 ? _a : "";
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+    const wordCount = [...words].length;
+    const readingTime = Math.max(1, Math.round(wordCount / 200));
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an article's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
+    // Support for API reference docs
+    const heading = article.querySelector("h1");
+    // Support for article docs with date
+    const date = (_b = article.querySelector("time")) === null || _b === void 0 ? void 0 : _b.parentNode;
+    const insertAfter = date !== null && date !== void 0 ? date : heading;
+    if (insertAfter) {
+        insertAfter.insertAdjacentElement("afterend", badge);
+    }
+}
+renderReadingTime(document.querySelector("article"));
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        // If a new article was added.
+        for (const node of mutation.addedNodes) {
+            if (node instanceof Element && node.tagName === "ARTICLE") {
+                // Render the reading time for this particular article.
+                renderReadingTime(node);
+            }
+        }
+    }
+});
+// https://developer.chrome.com/ is a SPA (Single Page Application) so can
+// update the address bar and render new content without reloading. Our content
+// script won't be reinjected when this happens, so we need to watch for
+// changes to the content.
+const devsiteContent = document.querySelector("devsite-content");
+if (devsiteContent) {
+    observer.observe(devsiteContent, {
+        childList: true,
+    });
+}
+//# sourceMappingURL=content.js.map
