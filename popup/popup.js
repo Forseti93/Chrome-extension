0608de72d0a5640c734edb255929dc2496cb46c5
Serhii Lavrenov <sergeylavrenov93@gmail.com>
2025-08-02 01:47:57 +0300

    npm run dev

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,24 @@
 "use strict";
 window.alert('Check the file "popup/popup.ts"');
 function textTypeChange(event) {
-  const target = event.target;
-  if (!target) return;
-  const selectedValue = target.value.toLowerCase();
-  switch (selectedValue) {
-    case "complex":
-      break;
-    case "simple":
-      break;
-    default:
-      break;
-  }
+    const target = event.target;
+    if (!target)
+        return;
+    const selectedValue = target.value.toLowerCase();
+    // TODO: make a speed variable changeable
+    // see the variable "readingTime". 150 or Complex text, 250 for simple text
+    // maybe storage API is suitable
+    switch (selectedValue) {
+        case "complex":
+            break;
+        case "simple":
+            break;
+        default:
+            break;
+    }
 }
 const textDifficultyElement = document.getElementById("text-difficulty");
 if (textDifficultyElement) {
-  textDifficultyElement.addEventListener("change", (e) => textTypeChange(e));
+    textDifficultyElement.addEventListener("change", (e) => textTypeChange(e));
 }
 //# sourceMappingURL=popup.js.map
